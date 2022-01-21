6abdd7a1ca36e8cfe177fca0c06975783d6ded3f
Zhao Zhang <zhao.zhang1@thoughtworks.com>
2022-01-21 14:40:42 +0800

    add hover and focus css

--- src/components/sub-menu/index.tsx
+++ src/components/sub-menu/index.tsx
@@ -3,18 +3,22 @@ import MenuItem, {MenuItemProps} from "../menu-item";
 
 import styles from "./index.module.scss";
 
+import classNames from "classnames/bind";
+
+const cx = classNames.bind(styles);
+
 export interface SubMenuProps {
     menuItems: MenuItemProps[];
+    currentMenuIndex? : number;
 }
 
 const SubMenu = (props: SubMenuProps) => {
-    const {menuItems} = props;
-    console.log(menuItems)
+    const {menuItems, currentMenuIndex} = props;
     return (
         <ul className={styles["sub-menu"]}>
             {menuItems.map((item: MenuItemProps, index) => {
                 return (
-                    <li key={index} className={styles["sub-menu-item"]}>
+                    <li key={index} className={cx(styles["item"], {"focus": index == currentMenuIndex}) }>
                         <MenuItem {...item} />
                     </li>
                 )
@@ -24,7 +28,8 @@ const SubMenu = (props: SubMenuProps) => {
 }
 
 SubMenu.defaultProps = {
-    menuItems: []
+    menuItems: [],
+    currentMenuIndex: 1
 }
 
 export default SubMenu;
